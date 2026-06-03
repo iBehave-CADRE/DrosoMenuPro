a5a3b59735489ea39ef67c384cc6e56003ab62a4
Benjamin Escribano <67735766+BenjaminEscribano@users.noreply.github.com>
2026-06-03 12:52:15 +0200

    firmware update

--- Arduino_Firmware/FeedingDevice/FeedingDevice.ino
+++ Arduino_Firmware/FeedingDevice/FeedingDevice.ino
@@ -6,6 +6,10 @@
 #define Button1Pin 7
 #define Button2Pin 8
 
+//Depending on if there is still an analog port free we can also just use an analog output
+#define BNCSynchOutPosition1Pin 9
+#define BNCSynchOutPosition2Pin 10
+
 //Defining variables for state memory and debouncing
 bool Button1StateOld;
 bool Button1StateNew;
@@ -24,30 +28,33 @@ bool BNCInputInOutStateNew;
 bool BNCInputPositionSelectStateOld;
 bool BNCInputPositionSelectStateNew;
 
-//Defining the absolute values for relevant positions
-long OutPosition = 0;
-long RestingPosition = 250000;
-//Positions of Stimuli relative to the Resting Position (Currently set to 8 mm distance)
-long Stimulus1Position = -41995;
-long Stimulus2Position = 41995;
-
-long CalibrationMarkerDistance = 94488; //Distance of 18 mm in Steps
-
-//Numper of Steps for a Distance = Distance in mm / Microstepsitze
-
-int MicrostepSize = 0.1905; // in µm 
-
 //MicrostepSize for X-LSM50B is 0.1905 µm
-//Knob Distance of 1 means 0.1905 µm per klick
-//Step Size is set to 0.5 mm
+const float MicrostepSize = 0.1905; // in µm/step
 
-long KnobStepSize = 1313;
+//Defining the absolute values for resting and out positions
+//those positions are setup specific and need to be adapted on a case to case basis
+//long xOutPosition = 0;
+//long zOutPosition = 5000;
+//long xRestingPosition = 250000;
+//long zStimulusHeightPosition = 50000;
+
+
+//Positions of Stimuli relative to the Resting Position (Currently set to 8 mm distance)
+//Those values are calibrated to the stimulus holder design dimenstions and ball radius
+const float xStimulus1Position = 5.0; //in mm
+const float xStimulus2Position = -5.0; //in mm
+const float xCalibrationMarkerDistance = 15.0; //Distance of 15 mm
+//Clearance distance is also setup specific
+const float zClearanceDistance = 3.0;
+
+//Knob Distance of 1 means 0.1905 µm per klick
+const float KnobStepSize = 0.25; // in mm
 
 //Defining the variables for storing the Device Address for serial communication
 int xAxisAddress = 1;
 int zAxisAddress = 2;
 
-String command = "";
+//String command = "";
 
 using namespace Zaber;
 
@@ -57,16 +64,28 @@ Shield shield(ZABERSHIELD_ADDRESS_AA);
 /* Create a connection object to control a connection using the shield */
 Connection connection(shield);
 
+int StepsInMM(long Steps, float uStepSize) {
+  int MM = Steps * (uStepSize / 1000.0);
+  Serial.println(String(MM));
+  return MM;
+}
+
+long MMinSteps(float MM, float uStepSize) {
+  long Steps = round(MM / (uStepSize / 1000));
+  //Serial.println(String(Steps));
+  return Steps;
+}
+
 void setup() {
   Serial.begin(9600);
   Serial.println("DrosoMenuPro Booting...");
 
-
-  // store current position (presist after reboot and power cycle)
-  //move to stored position
-
   pinMode(Button1Pin, INPUT);
   pinMode(Button2Pin, INPUT);
+  pinMode(BNCSynchOutPosition1Pin, OUTPUT);
+  digitalWrite(BNCSynchOutPosition1Pin, LOW);
+  pinMode(BNCSynchOutPosition2Pin, OUTPUT);
+  digitalWrite(BNCSynchOutPosition1Pin, LOW);
 
   //Using an external pulldown, better behavior of DrosoMenuPro
   pinMode(BNCInputInOutPin, INPUT);
@@ -75,30 +94,36 @@ void setup() {
   /* Initialize Zaber ASCII shield */
   shield.begin();
 
+  connection.genericCommand("renumber", xAxisAddress);
+
+  //Turning off LEDs to avoid PMT damage
+  Serial.println("Turning off the lamps for 2P imaging...");
+  connection.genericCommand("set system.led.enable 0", xAxisAddress);
+  connection.genericCommand("set system.led.enable 0", zAxisAddress);
+
   Serial.println("Setting Knob Mode...");
   connection.genericCommand("set knob.enable 1", xAxisAddress);
+  connection.genericCommand("set knob.enable 2", zAxisAddress);
   //Knob mode 0 is Velocity Mode 1 is Displacement Mode 2 is Force Mode
   //Set Knob mode to Displacement Mode
   connection.genericCommand("set knob.mode 1", xAxisAddress);
+  connection.genericCommand("set knob.mode 1", zAxisAddress);
 
-  connection.genericCommand("set knob.distance " + String(KnobStepSize) + "", xAxisAddress);
+  connection.genericCommand("set knob.distance " + String(MMinSteps(KnobStepSize, MicrostepSize)) + "", xAxisAddress);
+  connection.genericCommand("set knob.distance " + String(MMinSteps(KnobStepSize, MicrostepSize)) + "", zAxisAddress);
 
-  /* Issue a home command to device 1 */
-  Serial.println("Homing xzAxis...");
-  connection.genericCommand("home", xAxisAddress);
-  connection.waitUntilIdle(xAxisAddress);
-  //Home z Axis
+  /* Issue a home command to device 1 and 2 */
+  //Serial.println("Homing xzAxis...");
+  //connection.genericCommand("home", xAxisAddress);
+  //connection.waitUntilIdle(xAxisAddress);
+  //connection.genericCommand("home", zAxisAddress);
+  //connection.waitUntilIdle(zAxisAddress);
 
-
-  Serial.println("Moving to Defined Resting Position...");
-  connection.genericCommand("move abs " + String(RestingPosition) + "", xAxisAddress);
-  connection.waitUntilIdle(xAxisAddress);
-
-  Serial.println("Storing Current Position to Controller Menory Slot 1...");
-  connection.genericCommand("tools storepos 1 current", xAxisAddress);
-  connection.waitUntilIdle(xAxisAddress);
-
-
+  //Serial.println("Moving to Defined Resting Position...");
+  //connection.genericCommand("move abs " + String(xRestingPosition) + "", xAxisAddress);
+  //connection.waitUntilIdle(xAxisAddress);
+  //connection.genericCommand("move abs " + String(zStimulusHeightPosition) + "", zAxisAddress);
+  //connection.waitUntilIdle(zAxisAddress);
 
   Serial.println("Ready for Experiments...");
 }
@@ -118,31 +143,64 @@ void MoveStage() {
   BNCInputPositionSelectStateNew = digitalRead(BNCInputPositionSelectPin);
 
   if (BNCInputInOutStateNew != BNCInputInOutStateOld) {
+    connection.genericCommand("tools parking unpark", zAxisAddress);
+    connection.genericCommand("tools parking unpark", xAxisAddress);
       if (BNCInputInOutStateNew==false) {
         Serial.println("Moving to Resting Position...");
-        //connection.genericCommand("move abs " + String(RestingPosition) + "", xAxisAddress);
+
+        //Setting BNC Synch Signal to LOW for both positions
+        digitalWrite(BNCSynchOutPosition1Pin, LOW);
+        digitalWrite(BNCSynchOutPosition2Pin, LOW);
+
+        connection.genericCommand("move rel -" + String(MMinSteps(zClearanceDistance, MicrostepSize))  +"", zAxisAddress);
+        connection.waitUntilIdle(zAxisAddress);
+
         connection.genericCommand("move stored 1", xAxisAddress);
         connection.waitUntilIdle(xAxisAddress);
-        delay(1000);
+
+        connection.genericCommand("move rel " + String(MMinSteps(zClearanceDistance, MicrostepSize))  +"", zAxisAddress);
+        connection.waitUntilIdle(zAxisAddress);
+
       } else if (BNCInputInOutStateNew==true) {
         if (BNCInputPositionSelectStateNew==false) {
         Serial.println("Moving to Position 1...");
-        connection.genericCommand("move rel " + String(Stimulus1Position)  +"", xAxisAddress);
+
+        connection.genericCommand("move rel -" + String(MMinSteps(zClearanceDistance, MicrostepSize))  +"", zAxisAddress);
+        connection.waitUntilIdle(zAxisAddress);
+
+        connection.genericCommand("move rel " + String(MMinSteps(xStimulus1Position, MicrostepSize))  +"", xAxisAddress);
         connection.waitUntilIdle(xAxisAddress);
-        delay(1000);
+
+        connection.genericCommand("move rel " + String(MMinSteps(zClearanceDistance, MicrostepSize))  +"", zAxisAddress);
+        connection.waitUntilIdle(zAxisAddress);
+
+        //Setting BNC Synch Signal to HIGH for position 1
+        digitalWrite(BNCSynchOutPosition1Pin, HIGH);
+        digitalWrite(BNCSynchOutPosition2Pin, LOW);
+
         } else if (BNCInputPositionSelectStateNew==true) {
         Serial.println("Moving to Position 2...");
-        connection.genericCommand("move rel " + String(Stimulus2Position)  +"", xAxisAddress);
+
+        connection.genericCommand("move rel -" + String(MMinSteps(zClearanceDistance, MicrostepSize))  +"", zAxisAddress);
+        connection.waitUntilIdle(zAxisAddress);
+
+        connection.genericCommand("move rel " + String(MMinSteps(xStimulus2Position, MicrostepSize))  +"", xAxisAddress);
         connection.waitUntilIdle(xAxisAddress);
-        delay(1000);
+
+        connection.genericCommand("move rel " + String(MMinSteps(zClearanceDistance, MicrostepSize))  +"", zAxisAddress);
+        connection.waitUntilIdle(zAxisAddress);
+
+        //Setting BNC Synch Signal to HIGH for position 2
+        digitalWrite(BNCSynchOutPosition1Pin, LOW);
+        digitalWrite(BNCSynchOutPosition2Pin, HIGH);
         }
-        
       }
   BNCInputInOutStateOld = BNCInputInOutStateNew;
   BNCInputPositionSelectStateOld = BNCInputPositionSelectStateNew;
+  connection.genericCommand("tools parking park", zAxisAddress);
+  connection.genericCommand("tools parking park", xAxisAddress);
   }
 }
-
 
 //Function for Creating a Calibration Button
 //Calibration Workflow: Move to Calibration marker.
@@ -158,54 +216,45 @@ void Button1Action() {
         //delay(1);
       } else if (Button1StateNew==false) {
           //Execution if Button is pushed
+          connection.genericCommand("tools parking unpark", zAxisAddress);
+          connection.genericCommand("tools parking unpark", xAxisAddress);
+
 
           Serial.println("Movement to Resting Position Relative to Calibration Marker...");
-          //Move Down
-          //Pause
-          connection.genericCommand("move rel " + String(CalibrationMarkerDistance)  +"", xAxisAddress);
-          connection.waitUntilIdle(xAxisAddress);
-          //Move Up
-          //Pause
 
+          connection.genericCommand("move rel -" + String(MMinSteps(zClearanceDistance, MicrostepSize))  +"", zAxisAddress);
+          connection.waitUntilIdle(zAxisAddress);
+
+          connection.genericCommand("move rel " + String(MMinSteps(xCalibrationMarkerDistance, MicrostepSize))  +"", xAxisAddress);
+          connection.waitUntilIdle(xAxisAddress);
+
+          connection.genericCommand("move rel " + String(MMinSteps(zClearanceDistance, MicrostepSize))  +"", zAxisAddress);
+          connection.waitUntilIdle(zAxisAddress);
+          //Sore z Axis Position
           Serial.println("Storing Current Position as New Resting Position to Controller Menory Slot 1...");
           connection.genericCommand("tools storepos 1 current", xAxisAddress);
-          //Sore z Axis Position
+
           connection.waitUntilIdle(xAxisAddress);
+
+          connection.genericCommand("tools parking park", zAxisAddress);
+          connection.genericCommand("tools parking park", xAxisAddress);
       }
   Button1StateOld = Button1StateNew;
   }
 }
 
+
+////We use park and unpark instead of homing procedure to avoid crashing into other devices under the microscope
 void Button2Action() {
   Button2StateNew = digitalRead(Button2Pin);
   if (Button2StateNew != Button2StateOld) {
       if (Button2StateNew==true) {
         //delay(1);
       } else if (Button2StateNew==false) {
-        Serial.println("Exiting DrosoMenuPro to Out Position");
-        //Serial.println(String(MMinSteps(20)));
-        //Serial.println(String(StepsInMM(20)));
-        connection.genericCommand("move abs " + String(OutPosition)  +"", xAxisAddress);
-        connection.waitUntilIdle(xAxisAddress);
+        Serial.println("Unparking for calibration...");
+        connection.genericCommand("tools parking unpark", zAxisAddress);
+        connection.genericCommand("tools parking unpark", xAxisAddress);
       }
   Button2StateOld = Button2StateNew;
   }
 }
-
-int StepsInMM (long Steps) {
-  int MM = Steps * MicrostepSize;
-  return MM;
-}
-
-long MMinSteps (int MM) {
-  long Steps = round(MM / MicrostepSize);
-  return Steps;
-}
-
-
-
-//ToDo:
-//- Add second axis movement with colision avoidance behavior
-//- Add a function to calculate mm in steps and vise versa
-
-
